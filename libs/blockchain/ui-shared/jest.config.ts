e7fa491838d351e7f172b060b7859f81513c4462
alexpozdnyakof <alexpozdnyakof@gmail.com>
2022-09-06 16:18:52 +0700

    test: add jest-dom

--- libs/blockchain/ui-shared/jest.config.ts
+++ libs/blockchain/ui-shared/jest.config.ts
@@ -7,4 +7,5 @@ export default {
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../../coverage/libs/blockchain/ui-shared',
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
 };
