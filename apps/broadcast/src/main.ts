43529710648f2369ef66e56e3c51145b188780ba
alexpozdnyakof <alexpozdnyakof@gmail.com>
2022-09-09 21:14:53 +0700

    refactor(broadcast): update logic for transactions broadcasting

--- apps/broadcast/src/main.ts
+++ apps/broadcast/src/main.ts
@@ -1,31 +1,35 @@
 import { App } from 'uWebSockets.js';
+import { checkForNewTx, getLatestTxs } from './app/tx-model';
 
 const port = process.env.port || 3334;
 let time = 0;
 let intervalId;
-const uwsApp = App().ws('/*', {
+const channelName = 'txs';
+
+const app = App().ws('/*', {
   open: (socket) => {
     console.log({ id: socket });
-    socket.subscribe('time');
-    if (intervalId > 0) {
-      intervalId = setInterval(
-        () => ((time = time + 1), socket.send(time.toString())),
-        1000
-      );
-    }
+    socket.subscribe(channelName);
+
+    app.publish(channelName, JSON.stringify(getLatestTxs()));
+
+    intervalId = setInterval(
+      () => (
+        (time = time + 1),
+        app.publish(channelName, JSON.stringify(checkForNewTx()))
+      ),
+      30000
+    );
   },
-  message: (socket, message, isBinary) => {
-    console.log('message');
-    // socket.send(message, isBinary, true);
+  message: (socket, message) => {
     socket.publish('time', message);
   },
   close: () => {
     console.log('close connection');
     clearInterval(intervalId);
-    // time = 0;
   },
 });
 
-uwsApp.listen(Number(port), () => {
+app.listen(Number(port), () => {
   console.log('Listening at http://localhost:' + port + '/api');
 });
