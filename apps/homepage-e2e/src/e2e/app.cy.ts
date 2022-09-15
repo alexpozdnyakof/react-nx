68c8127a51007e662629f0f89ee89ebca1cb6b1f
alexpozdnyakof <alexpozdnyakof@gmail.com>
2022-09-16 03:12:14 +0700

    test(homepage-e2e) add latest blocks test

--- apps/homepage-e2e/src/e2e/app.cy.ts
+++ apps/homepage-e2e/src/e2e/app.cy.ts
@@ -5,9 +5,14 @@ describe('homepage', () => {
 
   it('should display welcome message', () => {
     // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
-
+    // cy.login('my-email@something.com', 'myPassword');
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome homepage');
+    getGreeting().contains('Latest Transactions');
+  });
+
+  it('should display last 10 txs', () => {
+    cy.get('[data-testid=table-body-row]').should((t) =>
+      expect(t.length).equal(10)
+    );
   });
 });
