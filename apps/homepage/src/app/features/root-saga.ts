01efc441ec353004a476e30e38cf6ddceb054b34
alexpozdnyakof <alexpozdnyakof@gmail.com>
2022-09-16 03:04:55 +0700

    feat(homepage): add transactions ws saga

--- apps/homepage/src/app/features/root-saga.ts
+++ apps/homepage/src/app/features/root-saga.ts
@@ -1,11 +1,11 @@
-import { call, fork, spawn } from 'redux-saga/effects';
-import { transactionsSaga } from './transactions';
+import { fork } from 'redux-saga/effects';
+import { latestBlockSaga } from './latest-blocks/saga';
 import { watchOnPings } from './transactions/ws-saga';
 
 export default function* rootSaga() {
   try {
     yield fork(watchOnPings);
-    yield fork(transactionsSaga);
+    yield fork(latestBlockSaga);
   } catch (e) {
     console.log('root level');
     console.log({ e });
--- apps/homepage/src/app/features/transactions/ws-saga.ts
+++ apps/homepage/src/app/features/transactions/ws-saga.ts
@@ -1,5 +1,6 @@
 import { eventChannel } from 'redux-saga';
-import { apply, call, delay, fork, put, take } from 'redux-saga/effects';
+import { call, put, take } from 'redux-saga/effects';
+import { loadSuccess } from './slice';
 
 const createWebSocketConnection = () => new WebSocket('ws://localhost:3334');
 
@@ -7,29 +8,21 @@ function createSocketChannel(socket: WebSocket) {
   socket.onopen = () => {
     console.log(socket);
     socket.send('hello');
-  };
+∏  };
 
-  // `eventChannel` takes a subscriber function
-  // the subscriber function takes an `emit` argument to put messages onto the channel
   return eventChannel((emit) => {
     const pingHandler = (event: MessageEvent) => {
-      // puts event payload into the channel
-      // this allows a Saga to take this payload from the returned channel
-      console.log(event.data);
       emit(event.data);
     };
 
     const errorHandler = (errorEvent: Event) => {
-      // create an Error object and put it into the channel
+      console.error({errorEvent});
       emit(new Error('socket error'));
     };
 
-    // setup the subscription
     socket.onmessage = pingHandler;
     socket.onerror = errorHandler;
 
-    // the subscriber must return an unsubscribe function
-    // this will be invoked when the saga calls `channel.close` method
     const unsubscribe = () => {
       socket.close();
     };
@@ -38,28 +31,18 @@ function createSocketChannel(socket: WebSocket) {
   });
 }
 
-// reply with a `pong` message by invoking `socket.emit('pong')`
-export function* pong(socket: WebSocket): any {
-  yield delay(5000);
-  yield apply(socket, socket.send, ['pong']); // call `emit` as a method with `socket` as context
-}
-
 export function* watchOnPings(): any {
   const socket = yield call(createWebSocketConnection);
   const socketChannel = yield call(createSocketChannel, socket);
 
   while (true) {
     try {
-      // An error from socketChannel will cause the saga jump to the catch block
       console.log('🏓 watching pings');
       const payload = yield take(socketChannel);
-      yield put({ type: 'INCOMING_PONG_PAYLOAD', payload });
-      yield fork(pong, socket);
+      yield put(loadSuccess({ data: JSON.parse(payload) }));
     } catch (err) {
       console.error('socket error:', err);
-      // socketChannel is still open in catch block
-      // if we want end the socketChannel, we need close it explicitly
-      // socketChannel.close()
+      socketChannel.close();
     }
   }
 }
