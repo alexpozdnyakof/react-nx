843b5e884711c8f81181c39754622d72a841894d
alexpozdnyakof <alexpozdnyakof@gmail.com>
2022-09-16 03:09:35 +0700

    refactor(homepage): minor changes

--- apps/homepage/src/app/features/transactions/slice.ts
+++ apps/homepage/src/app/features/transactions/slice.ts
@@ -1,33 +1,30 @@
+import { Tx } from '@blockchain/api-interfaces';
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
-type Transaction = {
-  id: string;
-  hash: string;
-}
-
 interface TransactionState {
-  txs: Array<Transaction>
-  status: 'idle' | 'loading' | 'failed'
+  txs: Array<Tx>;
+  status: 'idle' | 'loading' | 'failed';
 }
 
 const initialState: TransactionState = {
   txs: [],
-  status: 'idle'
-}
+  status: 'idle',
+};
 
 export const transactionsSlice = createSlice({
   name: 'transaction',
   initialState,
   reducers: {
-    load: (state) => { state.status = 'loading'},
-    loadSuccess: (state, action: PayloadAction<{data: Array<Transaction>}>) => {
+    load: (state) => {
+      state.status = 'loading';
+    },
+    loadSuccess: (state, action: PayloadAction<{ data: Array<Tx> }>) => {
       state.txs = [...action.payload.data, ...state.txs];
-      state.status = 'idle'
+      state.status = 'idle';
+    },
+  },
+});
 
-    }
-  }
-})
+export const { load, loadSuccess } = transactionsSlice.actions;
 
-export const { load, loadSuccess } = transactionsSlice.actions
-
-export default transactionsSlice.reducer
+export default transactionsSlice.reducer;
--- apps/homepage/src/app/features/transactions/transactions.tsx
+++ apps/homepage/src/app/features/transactions/transactions.tsx
@@ -1,5 +1,8 @@
+import { TxsTable, Widget } from '@blockchain/blockchain/ui-shared'
 import { useEffect } from 'react'
+import { useSelector } from 'react-redux'
 import { useAppDispatch } from '../../store-hooks'
+import { selectTxProps } from './selectors'
 import { load } from './slice'
 
 export default function TransactionsList() {
@@ -10,5 +13,16 @@ export default function TransactionsList() {
 
   }, [dispatch])
 
-  return <div>Transaction</div>
+  const txs = useSelector(selectTxProps)
+
+  return (
+    <Widget
+      title="Latest Transactions"
+      subtitle="Bitcoin"
+      symbol="https://www.blockchain.com/explorer/_next/static/media/btc.a6006067.png"
+      link="https://www.blockchain.com/btc/unconfirmed-transactions"
+    >
+      <TxsTable txs={txs}/>
+    </Widget>
+  )
 }
